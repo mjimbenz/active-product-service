25880c44beca98d5836f0a7a103b94c35eb59b9f
mjimbenz <MiguelAutberto.JimenezBenzol@emeal.nttdata.com>
2026-03-22 11:06:37 -0500

    chore(init): create spring boot base project

---
+++ src/main/java/com/bank/active_product/ActiveProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.active_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/active_product/ActiveProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.active_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
